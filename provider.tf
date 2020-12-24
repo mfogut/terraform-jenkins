f6bad4be7fe5321428b4ee8ccc7a0531495ffece
mfogut <fatihogut27@gmail.com>
2020-12-24 10:26:42 -0500

    backend added

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
     region = var.aws_region
 }
+
+terraform {
+    backend "s3" {
+        bucket = "fatih-terraform-backup"
+        key = "terraform.tfstate"
+        region = var.aws_region
+        dynamodb_table = "tf-backup"
+    }
+}
