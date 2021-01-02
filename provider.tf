48492689b7578541e20fdeaa2f53de2cfb0f8abf
mfogut <fatihogut27@gmail.com>
2021-01-01 21:40:11 -0500

    test

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ provider "aws" {
     region = "us-east-1"
 }
 
-terraform {
-    backend "s3" {
-        bucket = "fatih-terraform-backup"
-        key = "terraform.tfstate"
-        region = "us-east-1"
-        dynamodb_table = "tf-backup"
-    }
-}
+#terraform {
+#    backend "s3" {
+#        bucket = "fatih-terraform-backup"
+#        key = "terraform.tfstate"
+#        region = "us-east-1"
+#        dynamodb_table = "tf-backup"
+#    }
+#}
