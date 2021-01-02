3ae68bbc558c1815e7b1eba7bb9b930511c1465f
mfogut <fatihogut27@gmail.com>
2021-01-01 22:29:30 -0500

    test

--- backend_config.tf
+++ backend_config.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket = var.s3_bucket
         key = var.key
-        region = var.aws_region
+        region = var.region
         dynamodb_table = var.dynamodb_table
     }
 }
